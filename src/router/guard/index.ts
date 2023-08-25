d89e25b9ab9b758949cc9d3697f93cfe3af4947c
yp910108 <1548018679@qq.com>
2023-08-25 17:34:44 +0800

    fix

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -1,17 +1,9 @@
 import type { Router } from 'vue-router'
 import { useTitle } from '@vueuse/core'
-import { useOsTheme, darkTheme, createDiscreteApi } from 'naive-ui'
 import { createPermissionGuard } from './permission'
 
 export default function createRouterGuard(router: Router) {
-  const osTheme = useOsTheme()
-  const theme = osTheme.value === 'dark' ? darkTheme : undefined
-  const { message } = createDiscreteApi(['message'], {
-    configProviderProps: { theme }
-  })
   router.beforeEach(async (to) => {
-    message.success('测试提示信息1', { duration: 20000 })
-    window.$message?.success('测试提示信息2', { duration: 20000 })
     window.$loadingBar?.start()
     return await createPermissionGuard(to)
   })
