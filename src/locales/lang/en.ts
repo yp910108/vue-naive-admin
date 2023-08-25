941da3940c8fd1f8579811beddcf5920192ed445
yp910108 <1548018679@qq.com>
2023-08-25 09:40:08 +0800

    i18n

--- src/locales/lang/en.ts
+++ src/locales/lang/en.ts
@@ -15,80 +15,6 @@ const locale: I18nType.Schema = {
     pleaseCheckValue: 'Please check the value is valid',
     action: 'Action'
   },
-  routes: {
-    dashboard: {
-      _value: 'Dashboard',
-      analysis: 'Analysis',
-      workbench: 'Workbench'
-    },
-    document: {
-      _value: 'Document',
-      vue: 'Vue Document',
-      vite: 'Vite Document',
-      naive: 'NaiveUI Document',
-      project: 'Project Document',
-      'project-link': 'Project Document(href)'
-    },
-    component: {
-      _value: 'Component',
-      button: 'Button',
-      card: 'Card',
-      table: 'Table'
-    },
-    plugin: {
-      _value: 'Plugin',
-      charts: {
-        _value: 'Chart',
-        echarts: 'ECharts',
-        antv: 'AntV'
-      },
-      copy: 'Copy',
-      editor: {
-        _value: 'Editor',
-        quill: 'Quill',
-        markdown: 'Markdown'
-      },
-      icon: 'Icon',
-      map: 'Map',
-      print: 'Print',
-      swiper: 'Swiper',
-      video: 'Video'
-    },
-    'auth-demo': {
-      _value: 'Auth Demo',
-      permission: 'Toggle Permission',
-      super: 'Super Auth'
-    },
-    function: {
-      _value: 'Function',
-      tab: 'System Tab'
-    },
-    exception: {
-      _value: 'Exception',
-      403: '403',
-      404: '404',
-      500: '500'
-    },
-    'multi-menu': {
-      _value: 'Multi Degree Menu',
-      first: {
-        _value: 'First Degree',
-        second: 'Second Degree',
-        'second-new': {
-          _value: 'Second Degree With Children',
-          third: 'Third Degree'
-        }
-      }
-    },
-    management: {
-      _value: 'System Management',
-      auth: 'Auth',
-      role: 'Role',
-      route: 'Route',
-      user: 'User'
-    },
-    about: 'About'
-  },
   layout: {
     settingDrawer: {
       title: 'Theme configuration',
@@ -214,6 +140,6 @@ const locale: I18nType.Schema = {
       }
     }
   }
-};
+}
 
-export default locale;
+export default locale
