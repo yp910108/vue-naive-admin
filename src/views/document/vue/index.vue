658e321a39a25507175f7d13872132b58f50133d
yp910108 <1548018679@qq.com>
2023-08-30 10:38:37 +0800

    document

---
+++ src/views/document/vue/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-full">
+    <iframe class="wh-full" :src="src"></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const src = ref('https://v3.cn.vuejs.org/')
+</script>
