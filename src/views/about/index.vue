01ad4e2653c5be14b1e866dcc3839f3fcf9409b0
yp910108 <1548018679@qq.com>
2023-08-31 11:52:06 +0800

    about

---
+++ src/views/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <n-space vertical :size="16">
+    <project-introduction />
+    <project-info />
+    <pro-dependency />
+    <dev-dependency />
+  </n-space>
+</template>
+
+<script setup lang="ts">
+import DevDependency from './dev-dependency.vue'
+import ProDependency from './pro-dependency.vue'
+import ProjectInfo from './project-info.vue'
+import ProjectIntroduction from './project-introduction.vue'
+</script>
+
+<style scoped></style>
