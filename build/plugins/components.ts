dc72147ceb9b183c14fd8755408635acdc9e9c7a
yp910108 <1548018679@qq.com>
2023-09-05 16:24:50 +0800

    fix

--- build/plugins/components.ts
+++ build/plugins/components.ts
@@ -1,11 +1,15 @@
+import SvgComponents from 'vite-plugin-svg-components'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default function components() {
-  return Components({
-    dts: 'src/typings/components.d.ts',
-    dirs: ['src/components', 'src/icons/components'],
-    extensions: ['vue', 'tsx'],
-    resolvers: [NaiveUiResolver()]
-  })
+  return [
+    SvgComponents(),
+    Components({
+      dts: 'src/typings/components.d.ts',
+      dirs: ['src/components', 'src/icons/components'],
+      extensions: ['vue', 'tsx'],
+      resolvers: [NaiveUiResolver()]
+    })
+  ]
 }
--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import unocss from 'unocss/vite'
-import icons from './icons'
 import components from './components'
 import compress from './compress'
 import mock from './mock'
@@ -15,8 +14,7 @@ export function setupVitePlugins(env: ImportMetaEnv) {
     vueJsx(),
     vueDevTools(),
     unocss(),
-    icons(),
-    components(),
+    ...components(),
     mock(env)
   ]
 
