9e65b7997adb791cab22045c1f9f036929c4669e
yp910108 <1548018679@qq.com>
2023-09-05 12:37:10 +0800

    fix

--- src/layouts/basic-layout/app-header/menu-collapse.vue
+++ src/layouts/basic-layout/app-header/menu-collapse.vue
@@ -4,7 +4,7 @@
     :inverted="theme.header.inverted"
     @click="appStore.toggleSiderCollapse"
   >
-    <icon-menu-unfold-left v-if="appStore.siderCollapse" class="text-16px" />
+    <icon-menu-unfold-right v-if="appStore.siderCollapse" class="text-16px" />
     <icon-menu-fold-left v-else class="text-16px" />
   </hover-container>
 </template>
