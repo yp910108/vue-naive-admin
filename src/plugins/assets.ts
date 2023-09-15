38803ba905d1bb4d37a2f3c56a9344ff51a0cd82
yp910108 <1548018679@qq.com>
2023-09-15 16:05:10 +0800

    fix: bug fix

--- src/plugins/assets.ts
+++ src/plugins/assets.ts
@@ -1,6 +1,5 @@
 import 'virtual:uno.css'
 import 'vfonts/Lato.css'
-import 'virtual:svg-icons-register'
 import '../styles/index.scss'
 
 /** import static assets: css js font and so on. - [引入静态资源，css、js 、字体文件等] */
