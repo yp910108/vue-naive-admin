215bcc2110bc6777352a09f7f1779ab3139b0f44
yp910108 <1548018679@qq.com>
2025-10-11 09:20:03 +0800

    commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,15 +2,16 @@ import type { App } from 'vue'
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import { transformRoutes } from '@/utils'
 import { routeData } from './route-data'
+import { scrollBehavior } from './scroll-behavior'
 import { createRouterGuard } from './guard'
 
 const { VITE_HASH_ROUTE = 'N', VITE_BASE_URL } = import.meta.env
 
-// TODO scrollBehavior
-export const router = createRouter({
+const router = createRouter({
   history:
     VITE_HASH_ROUTE === 'Y' ? createWebHashHistory(VITE_BASE_URL) : createWebHistory(VITE_BASE_URL),
-  routes: transformRoutes(routeData)
+  routes: transformRoutes(routeData),
+  scrollBehavior
 })
 
 export const setupRouter = async (app: App) => {
