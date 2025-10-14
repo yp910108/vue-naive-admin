7588855296d3ebc949c603094ecd1ea86e6538b0
yp910108 <1548018679@qq.com>
2025-10-14 11:30:02 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <n-config-provider
     abstract
     inline-theme-disabled
+    :locale="zhCN"
+    :date-locale="dateZhCN"
     :theme="naiveTheme"
     :theme-overrides="naiveThemeOverrides"
   >
@@ -12,6 +14,7 @@
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
+import { zhCN, dateZhCN } from 'naive-ui'
 import { useThemeStore, subscribeStore } from './store'
 
 const { naiveTheme, naiveThemeOverrides } = storeToRefs(useThemeStore())
