d32dae70057dd8d5fbcf83f050abee959a273393
yp910108 <1548018679@qq.com>
2025-10-15 21:11:44 +0800

    commit

--- src/store/subscribe/tab.ts
+++ src/store/subscribe/tab.ts
@@ -1,10 +1,14 @@
 import { localStg } from '@/utils'
-import { useTabStore } from '../modules'
+import { useSettingsStore, useTabStore } from '../modules'
 
 export const subscribeTabStore = () => {
+  const settingsStore = useSettingsStore()
+
   const tabStore = useTabStore()
 
   tabStore.$subscribe((_, { tabs }) => {
-    localStg.set('tabs', tabs)
+    if (settingsStore.settings.tab.isCache) {
+      localStg.set('tabs', tabs)
+    }
   })
 }
